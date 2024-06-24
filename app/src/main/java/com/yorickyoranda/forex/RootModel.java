6e5ea6172519ff9d340258f764da78c79ae41e17
ezirs <64509305+ezirs@users.noreply.github.com>
2024-06-24 11:10:53 +0700

    first_commit

---
+++ app/src/main/java/com/yorickyoranda/forex/RootModel.java
@@ -0,0 +1,11 @@
+package com.yorickyoranda.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+    public RootModel() {}
+    public RatesModel getRatesModel() { return ratesModel; }
+    public void setRatesModel(RatesModel ratesModel) { this.ratesModel = ratesModel; }
+}
